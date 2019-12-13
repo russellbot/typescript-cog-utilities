35852e7cc0fe335d647b6c1e819d5906395e6367
Angelito Salud <asalud@you-source.com>
2019-12-13 10:22:24 +0800

    included date format in date error message

--- src/constants/errors/invalid-operand.ts
+++ src/constants/errors/invalid-operand.ts
@@ -1,6 +1,6 @@
 export class InvalidOperandError extends Error {
   constructor(operator: string, actualValue: string, value: string) {
-    super(`Couldn't check if ${actualValue} should ${operator} ${value}. The "${operator}" operator can only be used with numeric or date values.`);
+    super(`Couldn't check if ${actualValue} should ${operator} ${value}. The "${operator}" operator can only be used with numeric or date values: date format: yyyy-mm-dd date time format: yyyy-mm-dd hh:mm:ss`);
     this.name = 'InvalidOperandError';
   }
 }
